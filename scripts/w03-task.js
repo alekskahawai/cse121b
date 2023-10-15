c628229429f9893eb575bcec0e512ba9e68e15ac
alekskahawai <vol17004@byui.edu>
2023-10-15 01:30:00 +0100

    w03

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -42,29 +42,42 @@ const multiplyNumbers = () => {
 document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
 
 /* Open Function Use - Divide Numbers */
-const multiply = (number1, number2) => number1 * number2;
+const divide = (number1, number2) => number1 / number2;
 
-const multiplyNumbers = () => {
-    let multiplyNumber1 = Number(document.querySelector('#factor1').value);
-    let multiplyNumber2 = Number(document.querySelector('#factor2').value);
+const divideNumbers = () => {
+    let divideNumber1 = Number(document.querySelector('#dividend').value);
+    let divideNumber2 = Number(document.querySelector('#divisor').value);
 
-    document.querySelector('#product').value = multiply(multiplyNumber1, multiplyNumber2);
+    document.querySelector('#quotient').value = divide(divideNumber1, divideNumber2);
 }
 
-document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
+document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
 
 /* Decision Structure */
+let currentDate = new Date();
+let currentYear = '';
+currentYear = currentDate.getFullYear();
+
+document.querySelector('#year').textContent = currentYear;
 
 
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+document.querySelector('#array').innerHTML = array.join(', ');
 
 /* Output Odds Only Array */
+document.querySelector('#odds').innerHTML = array.filter(number => number % 2 !== 0).join(', ');
 
 /* Output Evens Only Array */
+document.querySelector('#evens').innerHTML = array.filter(number => number % 2 === 0).join(', ');
 
 /* Output Sum of Org. Array */
+document.querySelector('#sumOfArray').innerHTML = array.reduce((sum, number) => sum + number);
 
 /* Output Multiplied by 2 Array */
+document.querySelector('#multiplied').innerHTML = array.map(number => number * 2).join(', ');
 
 /* Output Sum of Multiplied by 2 Array */
+document.querySelector('#sumOfMultiplied').innerHTML = array.map(number => number * 2).reduce((sum, number) => sum + number);
