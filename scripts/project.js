c24b77a50c4ecd406931c262aed314ae106061c7
alekskahawai <vol17004@byui.edu>
2023-10-27 01:16:10 +0100

    w07

--- scripts/project.js
+++ scripts/project.js
@@ -71,23 +71,33 @@ const sortBy = templeList => {
     reset();
 
     const filter = document.querySelector('#sortBy').value;
-    const usa = [" Hawaii", " CA", " California", " Nevada", " Alaska"]
+    const usa = [' Alabama', ' Alaska', ' Arizona', ' Arkansas', ' California', ' Colorado', ' Connecticut',
+        ' Delaware', ' Florida', ' Georgia', ' Hawaii', ' Idaho', ' Illinois', ' Indiana', ' Iowa', ' Kansas',
+        ' Kentucky', ' Louisiana', ' Maine', ' Maryland', ' Massachusetts', ' Michigan', ' Minnesota',
+        ' Mississippi', ' Missouri', ' Montana', ' Nebraska', ' Nevada', ' New Hampshire', ' New Jersey',
+        ' New Mexico', ' New York', ' North Carolina', ' North Dakota', ' Ohio', ' Oklahoma', ' Oregon',
+        ' Pennsylvania', ' Rhode Island', ' South Carolina', ' South Dakota', ' Tennessee', ' Texas', ' Utah',
+        ' Vermont', ' Virginia', ' Washington', ' West Virginia', ' Wisconsin', ' Wyoming', ' AL', ' AK', ' AS', ' AZ', ' AR', ' CA', ' CO', ' CT', ' DE', ' DC', ' FM', ' FL', ' GA', ' GU', ' HI', ' ID', ' IL', ' IN', ' IA', ' KS', ' KY', ' LA', ' ME', ' MH', ' MD', ' MA', ' MI', ' MN', ' MS', ' MO', ' MT', ' NE', ' NV', ' NH', ' NJ', ' NM', ' NY', ' NC', ' ND', ' MP', ' OH', ' OK', ' OR', ' PW', ' PA', ' PR', ' RI', ' SC', ' SD', ' TN', ' TX', ' UT', ' VT', ' VI', ' VA', ' WA', ' WV', ' WI', ' WY', 'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
+        'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas',
+        'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+        'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
+        'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon',
+        'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah',
+        'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 
     switch (filter) {
 
         case "utah":
             displayTemples(
                 /* data.features.filter(item => item.properties.place.split(",")[1] == ' Hawaii') */
-                data.features.filter(item => usa.includes(item.properties.place.split(",")[1]))
-            );
+                data.features.filter(item => usa.includes(item.properties.place.split(",")[1]) || usa.includes(item.properties.place.split(" ")[1])) || usa.includes(item.properties.place));
 
             break;
 
         case "notutah":
             displayTemples(
                 /* data.features.filter(item => !item.location.includes('Utah')) */
-                data.features.filter(item => !usa.includes(item.properties.place.split(",")[1]))
-            );
+                data.features.filter(item => !usa.includes(item.properties.place.split(",")[1])) || !usa.includes(item.properties.place));
 
             break;
 
